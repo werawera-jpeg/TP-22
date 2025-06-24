4e562f2366a1c5c52706c5ca700386aa828784e9
Osamu95 <jonathanandriamalaza@gmail.com>
2025-06-25 00:24:38 +0300

    YES

---
+++ pages/modele.php
@@ -0,0 +1,18 @@
+<?php require("../inc/fonction.php")
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>employees</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+    <?php include ($_GET['page']) ?>
+
+</body>
+<footer>
+    <p>Copyleft 2025</p>
+</footer>
+</html>
